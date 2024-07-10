982209a92f579bf3f8dd89fdc0c792c1a4cc132c
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-10 19:01:14 +0530

    Added methods to perform complete CRUD operations.

--- standAloneJPA/src/main/java/com/boot/standAlone/StandAloneJpaApplication.java
+++ standAloneJPA/src/main/java/com/boot/standAlone/StandAloneJpaApplication.java
@@ -1,5 +1,8 @@
 package com.boot.standAlone;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,8 +22,70 @@ public class StandAloneJpaApplication {
 		user.setCity("Jaunpur");
 		user.setStatus("highly Motivated");
 		
+		/* inserting a single user */
+		/*
 		User user1 = userRepository.save(user);
 		System.out.println(user1);
+		*/
+		
+		User user2 = new User();
+		user2.setName("Shivangi Modanwal");
+		user2.setCity("Rampur");
+		user2.setStatus("marries");
+		
+		/* Inserting multiple users */
+		
+		List<User> users = List.of(user,user2);
+		Iterable<User> result = userRepository.saveAll(users);
+		
+		result.forEach(e->System.out.println(e));
+		
+		
+		/* selecting a single user */
+		/*
+		Optional<User> optional = userRepository.findById(3);
+		User forUpdate = optional.get();
+		
+		System.out.println(forUpdate);
+		*/
+		
+		/* selecting multiple users */
+		/*
+		Iterable<User> itr = userRepository.findAll();
+		itr.forEach(s->System.out.println(s));
+		*/
+		
+		/* updating single user */
+		/*
+		Optional<User> optional = userRepository.findById(2);
+		User forUpdate = optional.get();
+		
+		forUpdate.setName("Rajan Bhaiya");
+		User afterUpdate = userRepository.save(forUpdate);
+		
+		System.out.println(afterUpdate);
+		*/
+		
+		/* deleting single user */
+		/*
+		Optional<User> optional = userRepository.findById(2);
+		User forDelete = optional.get();
+		userRepository.delete(forDelete);
+		System.out.println("deletion performed successfully.");
+		// to check the list after deletion
+		Iterable<User> itr = userRepository.findAll();
+		itr.forEach(s->System.out.println(s));
+		*/
+		
+		/* deleting multiple users */
+		/*
+		Iterable<User> itr = userRepository.findAll();
+		userRepository.deleteAll(itr);
+		System.out.println("All the rows of user_details table gets deleted.");
+		*/
+		
+		
+		
 	}
 
 }
